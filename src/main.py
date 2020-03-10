a120c47fe59714305ea0fcb21e7917a43a249be4
aleaguilar <alejandroalbertoaguilar@gmail.com>
2020-03-10 00:59:27 +0000

    jwt + registation

--- src/main.py
+++ src/main.py
@@ -7,8 +7,11 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Customer, Queries, Products, Orders, Cart
 from activecampaign.client import Client
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
 
 #from models import Person
 
@@ -17,6 +20,8 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'secret-key'  # Change this!
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -39,6 +44,74 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/customers', methods=['GET'])
+def handle_customers():
+    all_customers = Customer.query.all()
+    all_customers = list(map(lambda x: x.serialize(), all_customers))
+    return jsonify(all_customers), 200
+
+@app.route('/register', methods=['POST'])
+def registration():
+
+    new_user = request.get_json()
+    print("new_user",new_user)
+
+    if 'name' not in new_user:
+        raise APIException ("Hey, we need to know who to deliver this to. Tell us your name", status_code=400)
+    
+    if 'lastname' not in new_user:
+        raise APIException ("We need your lastname too", status_code=400)
+    
+    if 'email' not in new_user:
+        raise APIException ("Email is always required!", status_code=400)
+
+    if 'address' not in new_user:
+        raise APIException ("You want your package, right? We need an address for that", status_code=400)
+
+    if 'password' not in new_user:
+        raise APIException ("Please set a password", status_code=400)
+    
+    user1 = Customer(name=new_user['name'], lastname=new_user['lastname'], email=new_user['email'], address=new_user['address'], city=new_user['city'], country=new_user['country'], password=new_user['password'])
+    print("user1", user1)
+    db.session.add(user1)
+    db.session.commit()
+
+    return jsonify({'message':"User Successfully Created"}), 200
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    params = request.get_json()
+    email = params.get('email', None)
+    password = params.get('password', None)
+
+    if not email:
+        return jsonify({"msg": "Please provide email"}), 400
+    if not password:
+        return jsonify({"msg": "Please provide a password"}), 400
+
+    if email != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    ret = {'jwt': create_jwt(identity=email)}
+    return jsonify(ret), 200
+
+
+# Protect a view with jwt_required, which requires a valid jwt
+# to be present in the headers.
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    return jsonify({'hello_from': get_jwt_identity()}), 200
+
+if __name__ == '__main__':
+    app.run()
 
 @app.route('/subscribe', methods=['POST'])
 def email_subscriber():
--- src/models.py
+++ src/models.py
@@ -3,16 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(25), nullable=False)
     lastname = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
-    phone = db.Column(db.String(15), nullable=True)
     address = db.Column(db.String(80), nullable=False)
     city = db.Column(db.String(25), nullable=False)
     country = db.Column(db.String(25), nullable=False)
     password = db.Column(db.String(80), nullable=False) #how to define the password column
-    cod_lib = db.Column(db.String(15), nullable=True)
     # order_history = relationship
     # search_history = relationship
 
@@ -24,55 +22,40 @@ class Customer(db.Model):
         "name": self.name,
         "lastname": self.lastname,
         "email": self.email,
-        "phone": self.phone,
         "address": self.address,
         "city": self.city,
         "country": self.country,
-        "cod_lib": self.cod_lib,
+        "password": self.password
         }
 
 class Queries(db.Model):
-    id = db.Column(db.Integer, unique=True)
-    session_id = db.Column(db.Integer, primary_key=True, unique=True) #how to define session duration and how to use date format plus numbers
+    id = db.Column(db.Integer, unique=True, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
     amz_asin = db.Column(db.String(25), nullable=False)
-    upc = db.Column(db.String(25), nullable=False)
-    product = db.Column(db.String(250), nullable=False)
-    description = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Integer(), nullable=False)
-    keywords = db.Column(db.String(250), nullable=False) #how to create an array for categories
-    categories = db.Column(db.String(250), nullable=False) #how to create an array for categories
-    fba = db.Column(db.Boolean(), nullable=True)
-    prohibited = db.Column(db.Boolean(), nullable=True)
-    ship_weight = db.Column(db.Integer, nullable=False) #how and where to do the math
-    ocean_dimension = db.Column(db.Integer, nullable=False) #how and where to do the math
-    air_dimension = db.Column(db.Integer, nullable=False) #how and where to do the math
-    added_cart = db.Column(db.Boolean(), nullable=True)
-    last_added_date = db.Column(db.DateTime, nullable=False)
+    upc = db.Column(db.String(25), nullable=False)
+    customer_id = db.Column(db.String(25), nullable=False)
     
     def __repr__(self):
-        return '<Search %r>' % self.order_num
+        return '<ASIN %r>' % self.amz_asin
 
     def serialize(self):
         return {
-        "product": self.product,
-        "description": self.description,
+        "amz_asin": self.amz_asin,
         "price": self.price,
-        "fba": self.fba,
-        "prohibited": self.prohibited,
-        "weight": self.ship_weight,
-        "cubic_feet": self.ocean_dimension,
-        "volumetric": self.air_dimension,
+        "upc": self.upc,
+        "user_id": self.user_id,
         }
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    order_temp_id = db.Column(db.Integer, unique=True)
+    order_id = db.Column(db.Integer, unique=True)
     date = db.Column(db.DateTime, nullable=False)
     prod_name = db.Column(db.String(250), nullable=False)
     quantity = db.Column(db.Integer, nullable=False) #pull from front-end
     price = db.Column(db.Integer(), nullable=False)
     ship_cost = db.Column(db.Integer, nullable=False) #how and where to do the math
+    ship_method = db.Column(db.String(25), nullable=False)
     order_total = db.Column(db.Integer(), nullable=False)
     
     def __repr__(self):
@@ -81,6 +64,8 @@ class Cart(db.Model):
     def serialize(self):
         return {
         "total": self.order_total,
+        "shipping": self.ship_cost,
+        "shipping method": self.ship_method,
         "id": self.order_temp_id,
         "quantity": self.quantity,
         "product": self.prod_name,
@@ -89,10 +74,10 @@ class Cart(db.Model):
 class Orders(db.Model):
     id = db.Column(db.Integer, unique=True)
     order_num = db.Column(db.Integer, primary_key=True, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.DateTime(), nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey("customer.id"), nullable=False)
-    subtotal = db.Column(db.Integer(), nullable=False) 
-    ship_amt = db.Column(db.Integer(), nullable=False)
+    subtotal = db.Column(db.Integer, nullable=False) 
+    ship_amt = db.Column(db.Integer, nullable=False)
     ship_method = db.Column(db.String(25), nullable=False)
     status = db.Column(db.String(15), nullable=True)
     receiver_code = db.Column(db.String(15), nullable=False)
@@ -113,3 +98,36 @@ class Orders(db.Model):
         "receiver": self.receiver_code,
         }
 
+class Products(db.Model):
+    id = db.Column(db.Integer, unique=True)
+    prod_name = db.Column(db.String(250), nullable=False)
+    amz_asin = db.Column(db.String(25), nullable=False, primary_key=True)
+    last_price = db.Column(db.Integer(), nullable=False)
+    upc = db.Column(db.String(25), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    keywords = db.Column(db.String(250), nullable=False) #how to create an array for categories
+    categories = db.Column(db.String(250), nullable=False) #how to create an array for categories
+    fba = db.Column(db.Boolean(), nullable=True)
+    prohibited = db.Column(db.Boolean(), nullable=True)
+    ship_weight = db.Column(db.Integer, nullable=False) #how and where to do the math
+    ocean_dimension = db.Column(db.Integer, nullable=False) #how and where to do the math
+    air_dimension = db.Column(db.Integer, nullable=False) #how and where to do the math
+
+    def __repr__(self):
+        return '<Product %r>' % self.prod_name
+
+    def serialize(self):
+        return {
+        "product name": self.prod_name,
+        "description": self.description,
+        "keywords": self.keywords,
+        "categories": self.categories,
+        "fba": self.fba,
+        "prohibited": self.prohibited,
+        "shipping weight": self.ship_weight,
+        "ocean volume": self.ocean_dimension,
+        "air volume": self.air_dimension,
+        }
+
+# class ProhibitedProducts(db.Model):
+# class ShippingRates(db.Model):
